aede1a5ffb298f76a7fc6b1efcc824ca1c08f265
Germán Gabriel <101677477+GermanGab1781@users.noreply.github.com>
2023-12-01 19:39:48 -0300

    Couple of retouches to HomePage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,9 @@
-import React, { useRef, useState, useEffect } from 'react';
-import { motion, useInView } from 'framer-motion';
+import React, { useState, useEffect } from 'react';
+import { motion } from 'framer-motion';
 import ReactImageGallery from 'react-image-gallery';
-import { useMediaQuery } from 'react-responsive'
-import { collection, query, orderBy, limit, getDocs, getDoc, doc } from 'firebase/firestore';
+/* import { useMediaQuery } from 'react-responsive' */
+import { collection, query, orderBy, getDocs, getDoc, doc } from 'firebase/firestore';
 import { db } from '../firebase';
-import { NavLink } from 'react-router-dom';
 import ItemHome from '../components/ItemHome';
 import ItemPreviewHome from '../components/ItemPreviewHome';
 const Home = () => {
@@ -12,22 +11,19 @@ const Home = () => {
   const [products, setProducts] = useState(undefined);
   /* Carousel */
   const [carousel, setCarousel] = useState(undefined);
-  const [slides, setSlides] = useState([])
-  const [latestPromo, setLatestPromo] = useState(undefined);
   /* Categories */
   const [categories, setCategories] = useState(undefined);
   const [categPreview, setCategPreview] = useState(undefined);
-  const initialCategPreview = {};
   /* Brands */
   const [brands, setBrands] = useState(undefined);
   const [brandsPreview, setBrandsPreview] = useState(undefined);
-  const initialBrandsPreview = {};
 
   useEffect(() => {
+    const initialCategPreview = {};
+    const initialBrandsPreview = {};
     const getAllDocs = async () => {
       const dataCategs = await getDocs(collection(db, "/products/Categories/list"))
       const dataProducts = await getDocs(query(collection(db, "/products/ProductsInfo/All"), orderBy('timestamp', 'asc')));
-      const dataLatest = await getDoc(doc(db, "homepage", "latest"));
       const dataCarousel = await getDoc(doc(db, "homepage", "carousel"));
       setCategories(dataCategs.docs)
       setProducts(dataProducts.docs.slice(0, 4));
@@ -48,51 +44,33 @@ const Home = () => {
       setBrands(Array.from(initialBrands))
       setBrandsPreview(initialBrandsPreview);
       setCategPreview(initialCategPreview);
-      setLatestPromo(dataLatest.data());
-      setCarousel(setImagesForCarousel(dataCarousel.data()))
+      setCarousel(Object.values(dataCarousel.data()).map(imageObj => ({
+        original: imageObj.Url,
+      })))
     }
     getAllDocs();
   }, [])
 
   /* Mobile check */
-  const isMobile = useMediaQuery({ query: "(max-width: 800px)" });
-
-  /* Slide Component */
-  const Slide = ({ Url }) => {
-    return <NavLink to="/catalog">
-      <div className='relative h-[65vh]'>
-        <img src={Url} className='bg-contain h-[65vh] w-full' alt='Slide Image' />
-        <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-white via-transparent to-transparent"></div>
-      </div>
-    </NavLink>
-  }
-
-  function setImagesForCarousel(list) {
-    let result = []
-    if (list.image1.Url !== "") {
-      result.push({ original: list.image1.Url, loading: "lazy", alt: "CarouselImage", originalClass: "h-full border cursor-auto" })
-    }
-    if (list.image2.Url !== "") {
-      result.push({ original: list.image2.Url, loading: "lazy", alt: "CarouselImage", originalClass: "h-full border cursor-auto" })
-    }
-    if (list.image3.Url !== "") {
-      result.push({ original: list.image3.Url, loading: "lazy", alt: "CarouselImage", originalClass: "h-full border cursor-auto" })
-    }
-    return result
-  }
-
+  /* const isMobile = useMediaQuery({ query: "(max-width: 800px)" }); */
 
   return (
-    <motion.div className='relative min-h-screen mb-60'>
+    <motion.div className='relative min-h-screen mb-60 bg-slate-100'>
       {/* Carousel */}
       {carousel
-        ? <ReactImageGallery items={carousel} showThumbnails={false} showFullscreenButton={false} showPlayButton={false} slideInterval={9000} autoPlay={false} additionalClass={'image-galleryHome'} />
+        ?
+        <ReactImageGallery items={carousel} renderItem={item =>
+          <div className="h-[75vh] overflow-hidden">
+            <img className="w-full h-full object-fill" src={item.original} alt="Promo" />
+            <div className="absolute bottom-0 left-0 w-full h-1/2 bg-gradient-to-t from-slate-100 via-transparent to-transparent"></div>
+          </div>
+        } showThumbnails={false} showFullscreenButton={false} showPlayButton={false} slideInterval={9000} autoPlay={false} additionalClass={'image-galleryHome'} />
         : <div className='relative bg-transparent w-full h-[65vh] text-4xl text-center animate-pulse border'>
           <span className='absolute top-1/4 left-1/2 transform -translate-x-1/2'>Loading</span>
         </div>
       }
 
-      <motion.div className='bg-transparent relative top-[30vh] '>
+      <motion.div className='bg-transparent relative top-[30vh]'>
         {/* Latest products */}
         <div>
           <h1 className='m-auto rounded-md w-fit text-4xl text-center p-2 bg-slate-200'>Latest</h1>
@@ -127,7 +105,7 @@ const Home = () => {
           </motion.div>
         </div>
         {/* Brands */}
-        <div>
+        <div className='bg-slate-100 pb-10'>
           <h1 className='text-center text-4xl mb-2'>Brands</h1>
           <motion.div className='relative flex gap-2 flex-wrap place-content-center '>
             {(brands && brandsPreview) &&
